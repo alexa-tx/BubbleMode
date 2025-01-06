05b0ea66b089d6e2f1bf81242ae844d1138d8090
sasha <sasha@HOME-PC>
2025-01-06 17:42:05 +0500

    main-section, about-section

---
+++ include/header.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="../assets/style.css">
+<header class="header">
+<span class="logo">BubbleMode</span>
+<nav>
+    <ul>
+        <li><a href="#main">Главная</a></li>
+        <li><a href="#about">О нас</a></li>
+        <li><a href="#">Напитки</a></li>
+        <li class="btn"><a href="#">Вход</a></li>
+    </ul>
+</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BubbleMode</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <?php include 'include/header.php'; ?>
+    <main class="main-section">
+        <div class="image-container">
+            <img src="../assets/images/bubblemode1.png" alt="BubbleMode">
+        </div>
+        <div class="info-section">
+            <h1>Добро пожаловать в BubbleMode!</h1>
+            <p>
+            Уютное кафе, где вы сможете насладиться 
+            вкуснейшими напитками с шариками тапиоки и другими уникальными вкусами. Мы предлагаем разнообразные 
+            напитки и десерты для любого настроения!
+            </p>
+        </div>
+    </main>
+
+    <section id="about" class="about-section">
+    <div class="about-content">
+        <div class="about-left">
+            <h2>О нас <i class='bx bx-coffee-togo'></i></h2>
+            <p>
+                BubbleMode — это уютное кафе, где каждый может попробовать уникальные напитки с шариками тапиоки и насладиться дружелюбной атмосферой. 
+                Наше кафе открыто для всех, кто любит оригинальные вкусы и необычные десерты.
+            </p>
+        </div>
+        <div class="about-right">
+            <h3>Наша миссия<i class='bx bx-edit-alt'></i></h3>
+            <p>
+                Мы стремимся подарить людям радость и новые вкусовые впечатления. Каждый напиток в нашем меню — это результат тщательного подбора ингредиентов и креативного подхода к созданию рецептов.
+            </p>
+        </div>
+    </div>
+</section>
+</body>
+</html>
